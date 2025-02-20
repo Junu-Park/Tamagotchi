d953fd96a7950981aad8f8f31d49b7c797917c82
Park Junwoo <ays99111@naver.com>
2025-02-20 18:36:47 +0900

    [chore] Color, Font, Image 디자인 시스템 추가

---
+++ Tamagotchi/Design/Extension+UIFont.swift
@@ -0,0 +1,30 @@
+//
+//  extension+UIFont.swift
+//  Tamagotchi
+//
+//  Created by 박준우 on 2/20/25.
+//
+
+import UIKit
+
+extension UIFont {
+    var system13: UIFont {
+        return UIFont.systemFont(ofSize: 13)
+    }
+    var system14: UIFont {
+        return UIFont.systemFont(ofSize: 14)
+    }
+    var system15: UIFont {
+        return UIFont.systemFont(ofSize: 15)
+    }
+    
+    var systemBold13: UIFont {
+        return UIFont.systemFont(ofSize: 13, weight: .bold)
+    }
+    var systemBold14: UIFont {
+        return UIFont.systemFont(ofSize: 14, weight: .bold)
+    }
+    var systemBold15: UIFont {
+        return UIFont.systemFont(ofSize: 15, weight: .bold)
+    }
+}
---
+++ Tamagotchi/Design/Extension+UIImage.swift
@@ -0,0 +1,26 @@
+//
+//  exteniosn.swift
+//  Tamagotchi
+//
+//  Created by 박준우 on 2/20/25.
+//
+
+import UIKit
+
+extension UIImage {
+    var eatFood: UIImage {
+        return UIImage(systemName: "leaf.circle")!
+    }
+    var drinkWater: UIImage {
+        return UIImage(systemName: "drop.circle")!
+    }
+    var setName: UIImage {
+        return UIImage(systemName: "pencil")!
+    }
+    var changeTamago: UIImage {
+        return UIImage(systemName: "moon.fill")!
+    }
+    var resetData: UIImage {
+        return UIImage(systemName: "arrow.clockwise")!
+    }
+}
