1bb6a2745a3ef0be66e6a1e76814391c5ad53687
Park Junwoo <ays99111@naver.com>
2025-02-20 18:38:11 +0900

    [feat] 다마고치 선택 화면 ViewController 생성

---
+++ Tamagotchi/View/SelectViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SelectViewController.swift
+//  Tamagotchi
+//
+//  Created by 박준우 on 2/20/25.
+//
+
+import UIKit
+
+final class SelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
