c1728c2bf57a059a739fefd968294265fa815817
Park Junwoo <ays99111@naver.com>
2025-02-23 15:24:42 +0900

    [feat] 메인 화면 ViewController 생성

---
+++ Tamagotchi/View/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Tamagotchi
+//
+//  Created by 박준우 on 2/23/25.
+//
+
+import UIKit
+
+final class MainViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
