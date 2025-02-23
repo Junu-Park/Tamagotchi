d5ecbe660c47de0fe5da7bf96935e92993baf3c7
Park Junwoo <ays99111@naver.com>
2025-02-23 23:09:18 +0900

    [feat] MainVC 네비게이션 우측 버튼 추가

--- Tamagotchi/View/Main/MainViewController.swift
+++ Tamagotchi/View/Main/MainViewController.swift
@@ -18,9 +18,17 @@ final class MainViewController: BaseViewController {
         let name = UserDataManager.name.value
         self.navigationItem.title = "\(name)님의 다마고치"
         
+        let right = UIBarButtonItem(image: .profile, style: .plain, target: self, action: #selector(self.transitionProfile))
+        self.navigationItem.setRightBarButton(right, animated: true)
+        
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = .background
         appearance.titleTextAttributes = [.foregroundColor: UIColor.font]
         self.navigationItem.scrollEdgeAppearance = appearance
+    }
+    
+    // TODO: 화면 전환 로직 추가
+    @objc private func transitionProfile() {
+        
     }
 }
